cb70ca376861d3198fbde4aa8085366870eff3bd
ljh01067 <leejh01067@gmail.com>
2025-11-27 22:45:56 +0900

    test1

---
+++ build.gradle.kts
@@ -0,0 +1,26 @@
+plugins {
+    id("java")
+}
+
+group = "org.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(17))   // JDK 17에 맞춤
+    }
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
