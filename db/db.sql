511cc471ef62b375a6eec02ac57212e9560e7f3c
ljh01067 <leejh01067@gmail.com>
2025-11-30 17:04:12 +0900

    db 수정과 admin.css 생성

--- db/db.sql
+++ db/db.sql
@@ -1,17 +1,22 @@
 DROP TABLE request;
 
 CREATE TABLE request (
-    id              BIGINT AUTO_INCREMENT PRIMARY KEY,
-    name            VARCHAR(50) NOT NULL,
-    contact         VARCHAR(50),
-    email           VARCHAR(100) NOT NULL,
-    work_scope      VARCHAR(200),
-    deadline        DATE,
-    genre           VARCHAR(100),
-    reference_link  VARCHAR(300),
-    note            TEXT,
-    guide_file      VARCHAR(300),
-    midi_file       VARCHAR(300),
-    reference_file  VARCHAR(300),
-    created_at      DATETIME DEFAULT CURRENT_TIMESTAMP
-);
+    id                 BIGINT AUTO_INCREMENT PRIMARY KEY,
+    NAME               VARCHAR(50) NOT NULL,
+    contact            VARCHAR(50),
+    email              VARCHAR(100) NOT NULL,
+    work_scope         VARCHAR(200),
+    deadline           DATE,
+    genre              VARCHAR(100),
+    reference_link     VARCHAR(300),
+    note               TEXT,
+    guide_file         LONGBLOB,
+    midi_file          LONGBLOB,
+    reference_file     LONGBLOB,
+    guide_filename     VARCHAR(300),
+    midi_filename      VARCHAR(300),
+    reference_filename VARCHAR(300),
+    created_at         DATETIME DEFAULT CURRENT_TIMESTAMP
+) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+SELECT * FROM request;
